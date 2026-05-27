4c41418869d46363e3e8bc2a70a933c061745a3a
Lilian Levano <zey.lilianlevano@gmail.com>
2026-05-27 11:56:37 +0200

    Initial commit

---
+++ src/main/java/com/example/werkstukentrepriseapplication/WerkstukEntrepriseApplication.java
@@ -0,0 +1,13 @@
+package com.example.werkstukentrepriseapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerkstukEntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerkstukEntrepriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/werkstukentrepriseapplication/WerkstukEntrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.werkstukentrepriseapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerkstukEntrepriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
