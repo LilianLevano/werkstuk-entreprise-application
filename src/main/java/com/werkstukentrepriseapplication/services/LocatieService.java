55497745a4062a23c961a6db764c206ee370ccbb
Lilian Levano <zey.lilianlevano@gmail.com>
2026-05-27 12:08:51 +0200

    ADD: LocatieService

---
+++ src/main/java/com/werkstukentrepriseapplication/services/LocatieService.java
@@ -0,0 +1,21 @@
+// LocatieService.java
+package com.werkstukentrepriseapplication.services;
+
+import com.werkstukentrepriseapplication.model.Locatie;
+import com.werkstukentrepriseapplication.repository.LocatieRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class LocatieService {
+
+    private final LocatieRepository repository;
+
+    public LocatieService(LocatieRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Locatie> getAlleLocaties() {
+        return repository.findAll();
+    }
+}
