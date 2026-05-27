6d126cd950768481dc23e3599899713a01cf9846
Lilian Levano <zey.lilianlevano@gmail.com>
2026-05-27 12:09:39 +0200

    ADD: EvenementService

---
+++ src/main/java/com/werkstukentrepriseapplication/services/EvenementService.java
@@ -0,0 +1,30 @@
+// EvenementService.java
+package com.werkstukentrepriseapplication.services;
+
+import com.werkstukentrepriseapplication.model.Evenement;
+import com.werkstukentrepriseapplication.repository.EvenementRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EvenementService {
+
+    private final EvenementRepository repository;
+
+    public EvenementService(EvenementRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Evenement> getLaatsteTien() {
+        return repository.findTop10ByOrderByTijdstipDesc();
+    }
+
+    public Evenement getById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Niet gevonden: " + id));
+    }
+
+    public void opslaan(Evenement evenement) {
+        repository.save(evenement);
+    }
+}
