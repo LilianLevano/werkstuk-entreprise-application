d538e8f0c7b7f0dee17c43fd3c0893370230f63e
Lilian Levano <zey.lilianlevano@gmail.com>
2026-05-27 12:01:07 +0200

    ADD: Locatie model

--- src/test/java/com/werkstukentrepriseapplication/WerkstukEntrepriseApplicationTests.java
+++ src/test/java/com/werkstukentrepriseapplication/WerkstukEntrepriseApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.werkstukentrepriseapplication;
+package com.werkstukentrepriseapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
